1031378065cd5b65543d2209c5c1254a8da106cc
Timur.Sharafutdinov <Timur.Sharafutdinov@south.rt.ru>
2020-08-01 18:25:49 +0300

    added tests for modelm released link between list elements and lists

--- TDD/urls.py
+++ TDD/urls.py
@@ -19,4 +19,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.home_page, name='home'),
+    path('lists/the_only_table/', views.view_list, name='view_list'),
+    path('lists/new', views.new_list, name='new_list'),
 ]
